5bef5973d2209fd09fa0c25e7d97306d480fb7cb
Sean Kearney <s.kearney.94@gmail.com>
2018-10-29 18:22:49 +0000

    Added cancel button while adding/editing hillforts Added ability to edit existing hillforts Add hillfort button changes to save button while editing

--- app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -2,6 +2,8 @@ package org.wit.hillfort.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
@@ -14,25 +16,51 @@ class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
     var hillfort = HillfortModel()
     lateinit var app : MainApp
+    var edit = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
         app = application as MainApp
-
+        toolbarAdd.title = title
+        setSupportActionBar(toolbarAdd)
+        if (intent.hasExtra("hillfort_edit")) {
+            edit = true
+            hillfort = intent.extras.getParcelable<HillfortModel>("hillfort_edit")
+            hillfortTitle.setText(hillfort.title)
+            description.setText(hillfort.description)
+            btnAdd.setText(R.string.save_hillfort)
+        }
         btnAdd.setOnClickListener() {
             hillfort.title = hillfortTitle.text.toString()
             hillfort.description = description.text.toString()
             if (hillfort.title.isNotEmpty()) {
-                app.hillforts.add(hillfort.copy())
+                if (edit) {
+                    app.hillforts.update(hillfort.copy())
+                } else {
+                    app.hillforts.create(hillfort.copy())
+                }
                 info("add Button Pressed: $hillfort")
-                app.hillforts.forEach{info("add Button Pressed: $hillfortTitle")}
+                //app.hillforts.findAll().forEach{info("add Button Pressed: $hillfortTitle")}
                 setResult(AppCompatActivity.RESULT_OK)
                 finish()
             }
             else {
-                toast ("Please Enter a title")
+                toast(R.string.enter_hillfort_title)
             }
         }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_hillfort, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.item_cancel -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/org/wit/hillfort/activities/HillfortListActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortListActivity.kt
@@ -1,18 +1,18 @@
 package org.wit.hillfort.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.*
 import kotlinx.android.synthetic.main.activity_hillfort_list.*
-import kotlinx.android.synthetic.main.card_hillfort.view.*
+import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.startActivityForResult
 import org.wit.hillfort.R
 import org.wit.hillfort.main.MainApp
 import org.wit.hillfort.models.HillfortModel
 
-class HillfortListActivity : AppCompatActivity() {
+class HillfortListActivity : AppCompatActivity(), HillfortListener {
 
     lateinit var app: MainApp
 
@@ -23,7 +23,7 @@ class HillfortListActivity : AppCompatActivity() {
 
         val layoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = layoutManager
-        recyclerView.adapter = HillfortAdapter(app.hillforts)
+        recyclerView.adapter = HillfortAdapter(app.hillforts.findAll(), this)
         toolbarMain.title = title
         setSupportActionBar(toolbarMain)
     }
@@ -39,26 +39,13 @@ class HillfortListActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
-}
 
-class HillfortAdapter constructor(private var hillforts: List<HillfortModel>) : RecyclerView.Adapter<HillfortAdapter.MainHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-        return MainHolder(LayoutInflater.from(parent?.context).inflate(R.layout.card_hillfort, parent, false))
+    override fun onHillfortClick(hillfort: HillfortModel) {
+        startActivityForResult(intentFor<HillfortActivity>().putExtra("hillfort_edit",hillfort), 0)
     }
 
-    override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val hillfort = hillforts[holder.adapterPosition]
-        holder.bind(hillfort)
-    }
-
-    override fun getItemCount(): Int = hillforts.size
-
-    class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        fun bind(hillfort: HillfortModel) {
-            itemView.hillfortTitle.text = hillfort.title
-            itemView.description.text = hillfort.description
-        }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        recyclerView.adapter?.notifyDataSetChanged()
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
