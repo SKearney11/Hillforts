e4fabda09b9f046b5ac54592217e99d181d209b9
Sean Kearney <s.kearney.94@gmail.com>
2018-10-29 20:07:14 +0000

    Added ability to add a picture to a hillfort or edit an existing picture

--- app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -1,5 +1,6 @@
 package org.wit.hillfort.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -9,14 +10,18 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.hillfort.R
+import org.wit.hillfort.helpers.readImage
+import org.wit.hillfort.helpers.readImageFromPath
+import org.wit.hillfort.helpers.showImagePicker
 import org.wit.hillfort.main.MainApp
 import org.wit.hillfort.models.HillfortModel
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
     var hillfort = HillfortModel()
-    lateinit var app : MainApp
+    lateinit var app: MainApp
     var edit = false
+    val IMAGE_REQUEST = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,37 +35,57 @@ class HillfortActivity : AppCompatActivity(), AnkoLogger {
             hillfortTitle.setText(hillfort.title)
             description.setText(hillfort.description)
             btnAdd.setText(R.string.save_hillfort)
-        }
-        btnAdd.setOnClickListener() {
-            hillfort.title = hillfortTitle.text.toString()
-            hillfort.description = description.text.toString()
-            if (hillfort.title.isNotEmpty()) {
-                if (edit) {
-                    app.hillforts.update(hillfort.copy())
-                } else {
-                    app.hillforts.create(hillfort.copy())
-                }
-                info("add Button Pressed: $hillfort")
-                //app.hillforts.findAll().forEach{info("add Button Pressed: $hillfortTitle")}
-                setResult(AppCompatActivity.RESULT_OK)
-                finish()
+            hillfortImage.setImageBitmap(readImageFromPath(this, hillfort.image))
+            if (hillfort.image != null) {
+                chooseImage.setText(R.string.change_hillfort_image)
             }
-            else {
-                toast(R.string.enter_hillfort_title)
+            btnAdd.setOnClickListener() {
+                hillfort.title = hillfortTitle.text.toString()
+                hillfort.description = description.text.toString()
+                if (hillfort.title.isNotEmpty()) {
+                    if (edit) {
+                        app.hillforts.update(hillfort.copy())
+                    } else {
+                        app.hillforts.create(hillfort.copy())
+                    }
+                    info("add Button Pressed: $hillfort")
+                    //app.hillforts.findAll().forEach{info("add Button Pressed: $hillfortTitle")}
+                    setResult(AppCompatActivity.RESULT_OK)
+                    finish()
+                } else {
+                    toast(R.string.enter_hillfort_title)
+                }
+            }
+            chooseImage.setOnClickListener {
+                showImagePicker(this, IMAGE_REQUEST)
             }
         }
     }
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_hillfort, menu)
-        return super.onCreateOptionsMenu(menu)
+        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+            menuInflater.inflate(R.menu.menu_hillfort, menu)
+            return super.onCreateOptionsMenu(menu)
+        }
+
+        override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+            when (item?.itemId) {
+                R.id.item_cancel -> {
+                    finish()
+                }
+            }
+            return super.onOptionsItemSelected(item)
+        }
+
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+            when (requestCode) {
+                IMAGE_REQUEST -> {
+                    if (data != null) {
+                        hillfort.image = data.getData().toString()
+                        hillfortImage.setImageBitmap(readImage(this, resultCode, data))
+                        chooseImage.setText(R.string.change_hillfort_image)
+                    }
+                }
+            }
+        }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when (item?.itemId) {
-            R.id.item_cancel -> {
-                finish()
-            }
-        }
-        return super.onOptionsItemSelected(item)
-    }
-}
